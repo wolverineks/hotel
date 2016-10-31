4b002227297487b6007badc1b1902058768ba45f
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-10-31 14:34:20 -0700

    Add comment to conflict detection algorithm

--- models/room.rb
+++ models/room.rb
@@ -12,10 +12,12 @@ class Room
     @id = self.class.count
   end
 
+  # Iterate through all of the rooms reservations, and check if there is any overlap
   def available?(start_date:, end_date:)
     !reservations.any? { |r| r.start_date <= end_date && r.end_date >= start_date }
   end
 
+  # The inverse of available?
   def booked?(start_date:, end_date:)
     !available?(start_date: start_date, end_date: end_date)
   end
