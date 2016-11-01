78c554646e9bd969a5aa571789d4d4e05cc76fcd
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-11-01 11:42:15 -0700

    Add comments to factory

--- factories/factory.rb
+++ factories/factory.rb
@@ -4,14 +4,17 @@ module Factory
   require './models/reservation.rb'
   require './models/room.rb'
 
+  # factory to create a guest
   def create_guest(first_name: ("A".."z").to_a.sample(5).join, last_name: ("A".."z").to_a.sample(5).join)
     Guest.new(first_name: first_name, last_name: last_name)
   end
 
+  # factory to create a room
   def create_room
     Room.new
   end
 
+  # factory to create a reservation for a given guest_id, room_id, and date range
   def create_reservation(guest_id:, room_id:, start_date:, end_date:)
     Reservation.new(
       guest_id: guest_id,
@@ -21,6 +24,7 @@ module Factory
     )
   end
 
+  # factory to create a guest, room, and reservation with the guest and room
   def create_trio(guest_id: create_guest.id, room_id: create_room.id, start_date: DateTime.now + 1, end_date: DateTime.now + 2)
     guest = create_guest
     room  = create_room
